ffb7f3277b672c002702a1b0d0edf59eef76cd01
ymkz <ymkzly@gmail.com>
2020-03-25 11:34:49 +0900

    chore: remove babel-preval due to next.js support static-build

--- packages/babel-preval/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = api => {
-  api.cache(true)
-  return {
-    presets: [['next/babel']],
-    plugins: [['macros'], ['module-resolver', { alias: { '~': './src' }, extensions: ['.js', '.ts', '.tsx'] }]],
-  }
-}
